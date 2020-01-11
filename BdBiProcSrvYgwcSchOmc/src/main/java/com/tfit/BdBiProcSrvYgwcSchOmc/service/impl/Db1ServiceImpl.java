e8f7a7bcfe0ba6d258aafd1f5dacd6f9551651d4
jianghy <haoyuan_jiang@ssic.cn>
2020-01-11 11:56:38 +0800

    修改

--- BdBiProcSrvYgwcSchOmc/src/main/java/com/tfit/BdBiProcSrvYgwcSchOmc/service/impl/Db1ServiceImpl.java
+++ BdBiProcSrvYgwcSchOmc/src/main/java/com/tfit/BdBiProcSrvYgwcSchOmc/service/impl/Db1ServiceImpl.java
@@ -589,7 +589,7 @@ public class Db1ServiceImpl implements Db1Service {
 			type = "1";//1:区报告
 		}
 		String reportName = startDate + "至 " + endDate + "周数据报告("+committeeName+")" ;
-		String file_url = projectPath +"bdbiprocsrvshygwcsch/biOptAnl/v1/downloadPdf?createDate="+createDate+"&committeeName="+committeeName;
+		String file_url = projectPath +"bdbiprocsrvshygwcsch/biOptAnl/v1/downloadPdf?createDate="+createDate+"&committeeCode="+committeeCode;
 //		String file_url = URL + ftpPath +committeeCode+"-"+createDate+".pdf";
 		String sql = "insert into t_week_data_report(id,year_date,month_date,committee_type,committee_code,committee_name,report_name,create_date,file_url) values('"+id+"','"+year+"','"+month+"','"+type+"','"+committeeCode+"','"+committeeName+"','"+reportName+"','"+createDate+"','"+file_url+"')";
 		logger.info("sql语句：" + sql);
