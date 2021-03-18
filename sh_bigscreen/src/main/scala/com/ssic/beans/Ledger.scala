b60559e3ed2f3ef6ba532572deec94904cf97ddb
sunzg <zhengguang_sun@ssic_cn>
2021-03-18 13:51:25 +0800

    “pdate .gitignore

--- sh_bigscreen/src/main/scala/com/ssic/beans/Ledger.scala
+++
@@ -1,9 +0,0 @@
-package com.ssic.beans
-
-class Ledger(
-            var master_id:String,
-            var delivery_attr:String,
-            var stat:String
-            ) {
-  override def toString: String = super.toString
-}
